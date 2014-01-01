6c7e5c229d17093890cba5fef47f19653f994c84
Charlie <apple@Apples-MacBook-Pro.local>
2014-01-01 13:06:15 +0530

    Add the ability to send push from the command line by asking the user

--- lib/pushofy/androidpush.rb
+++ lib/pushofy/androidpush.rb
@@ -5,12 +5,14 @@ require 'uri'
 #
 module Pushofy
   class AndroidPush
-    def initialize(body)
+    def initialize(body,api_key)
+      @api_key = api_key
       @body = body
     end
     def push
       uri = URI('https://android.googleapis.com/gcm/send')
-      auth_key = 'key=AIzaSyCoTPiIlC9OKuLh6i66focOp3zygWWOKXs'
+      auth_key = "key=#{@api_key}"
+      p auth_key
       headers = { 'Content-Type' => 'application/json',
                   'Authorization' => auth_key }
       http = Net::HTTP.new(uri.host, uri.port)
